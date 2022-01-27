84d3f56d2f1e48f1cab9023f71654b29d706bc4b
Samaïna <sasaclariss@gmail.com>
2022-01-27 16:07:23 +0100

    connexion user

--- connexion/index.php
+++ connexion/index.php
@@ -2,6 +2,14 @@
 
 <?php
 
-echo "<h1>Connexion</h1>"
+echo "<h1>Connexion</h1>";
+
+echo "<form method='post' action='action-login.php'>
+<label>Identifiant</label><input type='text' name='login'><br><br>
+<label>Mot de passe</label><input type='password' name='mdp'><br><br>
+<input type='submit' value='Connexion'>
+</form>";
+
+echo "<br><br><p>Première connexion ? <a href='inscription.php'>Incrivez-vous</a></p>";
 
 ?>
