1c97413c13573ea52f16b76c5c584dc169b77d0c
Léo Meyne <leo.meyne@gmail.com>
2022-01-27 16:27:37 +0100

    léo

--- connexion/action-insc.php
+++ connexion/action-insc.php
@@ -1,1 +1,20 @@
 <?php
+if(isset($_POST['login'],$_POST['mdp'],$_POST['mdpConfirm'])){
+    foreach ($_POST as $k => $v){
+        $$k=$v;
+    }
+
+    if ($mdp==$mdpConfirm){
+
+      $file="../data/users.csv";
+      $fp=fopen($file,"a") or die("impossible de trouver le fichier");
+      fputcsv($fp,[$login,md5($mdp)]);
+      fclose($fp);
+        header('location: inscription.php');
+    }
+    else {
+        header('location: inscription.php?id=1');
+    }
+}
+
+?>
