70e161730c2e8ba7f6cc1d8b6f0c02b7043b7cab
Samaïna <sasaclariss@gmail.com>
2022-01-27 15:05:48 +0100

    css

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+
+echo "<head><link href='bootstrap.css' rel='stylesheet' type='text/css' media='all'/></head>";
+
+echo "<nav></nav>";
+
+echo "<form method='POST' action='connexion/index.php'>
+<input type='button' value='Connexion' class='m-2'>
+</form>";
+
+
+echo "<p></p>";
+
+?>
