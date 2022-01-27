88a5dc1dbe42602bfb0e6631b400180c5e00101f
Samaïna <sasaclariss@gmail.com>
2022-01-27 18:08:27 +0100

    inscription

--- connexion/action-insc.php
+++ connexion/action-insc.php
@@ -1,20 +1,51 @@
 <?php
 if(isset($_POST['login'],$_POST['mdp'],$_POST['mdpConfirm'])){
-    foreach ($_POST as $k => $v){
-        $$k=$v;
+
+//    if (isset ($_POST['g-recaptcha-response']) &&! empty ($_POST ['g-recaptcha-response'])) {
+//        $secret = '6LfQ4T4eAAAAAJPodQ0tmgGKRf778KcM-lh3cJrx';
+//        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+//        $responseData = json_decode ($verifyResponse);
+//        if ($responseData-> success) {
+//            header('Location: inscription.php?id=3');
+//        }
+//        else {header('Location: inscription.php?id=4'); }
+//    }
+
+    $login = $_POST['login'];
+    $mdp = $_POST['mdp'];
+    $mdpConfirm = $_POST['mdpConfirm'];
+    $rep = $_POST['rep'];
+
+    echo $login, $mdp, $mdpConfirm, $rep;
+
+    if ($rep != 7) {
+        header('Location: inscription.php?id=3');
     }
 
-    if ($mdp==$mdpConfirm){
+    if ($mdp != $mdpConfirm){
+        header('Location: inscription.php?id=1');
+    }
 
-      $file="../data/users.csv";
-      $fp=fopen($file,"a") or die("impossible de trouver le fichier");
-      fputcsv($fp,[$login,md5($mdp)]);
-      fclose($fp);
-        header('location: inscription.php');
+    $file = fopen("../data/users.csv","a") or die("Impossible de trouver le fichier");
+    $flag = true;
+    while ($line = fgetcsv($file)) {
+        if ($login == $line[0])
+            $flag = false;
     }
-    else {
-        header('location: inscription.php?id=1');
+    fclose($file);
+
+    if ($flag){
+      $file = fopen("../data/users.csv","a") or die("Impossible de trouver le fichier");
+      fputcsv($file,[$login,md5($mdp)]);
+      fclose($file);
+//      header('Location: inscription.php');
     }
+
+    else
+        header('Location: inscription.php?id=2');
+
 }
+else
+    header('Location: inscription.php?id=4');
 
 ?>
--- connexion/inscription.php
+++ connexion/inscription.php
@@ -6,48 +6,60 @@
 </head>
 <body>
 
-  <?php
-  if(isset($_GET['id'])){
-      switch  ($_GET['id']){
-          case '1':
-              echo"<p style='color: red;'>mot de passe incorecte</p>";
-              break;
+<h1>Inscription professeur</h1>
+
+<?php
+    if(isset($_GET['id'])){
+        switch  ($_GET['id']){
+            case '1':
+                echo"<p style='color: red;'>Mot de passe incorrect</p>";
+                break;
+            case '2':
+                echo"<p style='color: red;'>Login déjà existant</p>";
+                break;
+            case '3':
+                echo"<p style='color: red;'>Captcha incorrect</p>";
+                break;
+        }
     }
-  }
-  ?>
+?>
 
-
-
-    <form action='action-insc.php' method='post'>
-    <table >
-      <tr>
-        <td>Identifiant :</td>
+<form action='action-insc.php' method='post'>
+    <table>
+        <tr>
+        <td>Identifiant</td>
         <td><input type='text' name='login'></td>
         <tr>
-          <td>mot de passe : </td>
+          <td>Mot de passe</td>
           <td><input type='password' name='mdp'></td>
         </tr>
         <tr>
-          <td>confirmer votre mot de passe : </td>
+          <td>Confirmer votre mot de passe</td>
           <td><input type='password' name='mdpConfirm'></td>
         </tr>
-        <td><script src="https://www.google.com/recaptcha/api.js" async defer></script>
-        <div class="g-recaptcha" data-sitekey="6LfQ4T4eAAAAAJcnH3h8L7ruen23i-T4jHuIZkEF"></div>
-        <td><button class="g-recaptcha" data-sitekey="6LfQ4T4eAAAAAJPodQ0tmgGKRf778KcM-lh3cJrx" data-callback='onReCaptchaValid'>Envoyer</button></td>
-        <script type="text/javascript">
-      function onReCaptchaValid(token) {
-        document.getElementById('id_du_formulaire').submit();
-      }
-    </script>
-    
-      </tr>
-      <tr>
-        <td></td>
-        <td> <input type='submit' name='bouton' value='Inscrit'></td>
-  </td>
 
-      </tr>
+        <tr>
+            <td>
+<!--        <script src="https://www.google.com/recaptcha/api.js" async defer></script>-->
+<!--        <div class="g-recaptcha" data-sitekey="6LfQ4T4eAAAAAJcnH3h8L7ruen23i-T4jHuIZkEF"></div>-->
+<!--        <button class="g-recaptcha" data-sitekey="6LfQ4T4eAAAAAJPodQ0tmgGKRf778KcM-lh3cJrx" data-callback='onReCaptchaValid'>Inscription</button>-->
+<!---->
+<!--        <script type="text/javascript">-->
+<!--        function onReCaptchaValid(token) {-->
+<!--            document.getElementById('id_du_formulaire').submit();-->
+<!--        }-->
+<!--        </script>-->
+                <label>5 + 2</label>
+            </td>
+            <td>
+                <input type="text" name="rep">
+            </td>
+        </tr>
+
     </table>
+    <br>
+    <input type="submit" value="Inscription">
+</form>
 
-  </form>
 </body>
+</html>
